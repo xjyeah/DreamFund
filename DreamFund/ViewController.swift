943863620125d2269b516bc42a2bed0d02d68408
ye.xiujiao <xjyeah@gmail.com>
2015-08-10 10:46:45 +0800

    Initial Commit

---
+++ DreamFund/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamFund
+//
+//  Created by Kelvin on 15/8/10.
+//  Copyright (c) 2015年 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
